6265690d9410ec77c3a8bec09eaeb2742140d67f
Stefano Colla <colla@master.local>
2026-02-10 10:47:22 +0100

    Moved key store outside classpath, on the file system, for easily update certs

--- cameras_api/src/main/java/it/serravalle/cameras_api/utils/BcryptGen.java
+++ cameras_api/src/main/java/it/serravalle/cameras_api/utils/BcryptGen.java
@@ -5,6 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BcryptGen {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("admin"));
+        System.out.println(encoder.encode("i9jPMgRxvxB"));
     }
 }
